fc8f6197a9e5ca8081f3d67502a01f2e4fecce51
Aryan Sarang <thearyansarang@gmail.com>
2023-08-26 15:36:27 +0530

    first commit

---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const HomePage = () => {
+    return (
+        <div>
+            Homeds
+            findsf
+            sendfd
+            sendfdsdf
+        </div>
+    )
+};
+
+export default HomePage;
